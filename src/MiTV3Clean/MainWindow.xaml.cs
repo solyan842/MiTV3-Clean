e1dcac143db2cd7d896d3d60a700bc1f98e15eba
SolYan <yansign842@gmail.com>
2026-09-19 21:41:03 +0700

    feat: wire Xiaomi HOME restore action

--- src/MiTV3Clean/MainWindow.xaml.cs
+++ src/MiTV3Clean/MainWindow.xaml.cs
@@ -131,6 +131,9 @@ public partial class MainWindow : Window
     private async void DiagnoseHome_Click(object sender, RoutedEventArgs e)
         => await GuardAsync(async () => Log(await _couchy.DiagnoseHomeAsync(_adb)));
 
+    private async void RestoreXiaomiHome_Click(object sender, RoutedEventArgs e)
+        => await GuardAsync(async () => Log(await _couchy.RestoreXiaomiHomeAsync(_adb)));
+
     private async Task ApplySelectedAsync(Func<PackageEntry, Task<string>> action, string title)
     {
         await GuardAsync(async () =>
